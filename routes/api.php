05672ad701db9562508c56107145689d5f2a8dc5
Kossy-Augusta <kossyudogu@gmail.com>
2024-07-22 12:21:29 +0100

    api routes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('v1/user/sign_up',[UserAuthcontroller::class, 'store']);
+Route::post('v1/user/login', [UserAuthcontroller::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::post('v1/user/logout', [UserAuthcontroller::class, 'destroy']);
+});
