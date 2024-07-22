e2fb6c387338e634548a739f325f7872816db82c
Kossy-Augusta <kossyudogu@gmail.com>
2024-07-22 15:12:27 +0100

    added route to dit image

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\UserAuthcontroller;
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,6 @@ Route::post('v1/user/sign_up',[UserAuthcontroller::class, 'store']);
 Route::post('v1/user/login', [UserAuthcontroller::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::post('v1/user/edit-picture', [UserProfileController::class, 'editImage']);
     Route::post('v1/user/logout', [UserAuthcontroller::class, 'destroy']);
 });
