76f80cd934f33ed0d547763bf3d127e9afdb293a
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-12 00:16:02 +0100

    post model

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description', 
+        'blog_image'
+    ];
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function category(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'category_post', 'post_id', 'category_id');
+    }
+}
