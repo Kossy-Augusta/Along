341da916bbeaa3d5ea33012d373f8a73eaf033c3
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-12 12:06:43 +0100

    fixes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\AccountTYpe;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -10,9 +11,9 @@ use App\Http\Requests\CreatePostRequest;
 
 class PostController extends Controller
 {
-    public function create(CreatePostRequest $request)
+    public function store(CreatePostRequest $request)
     {
-        $credentials = $request->only('title', 'description', 'blog_image');
+        $credentials = $request->only('title', 'description', 'blog_image', "status");
         $image = $credentials['blog_image'];
         $path = $image->store('blog_images', 'public');
         $credentials['blog_image'] = $path;
@@ -20,8 +21,8 @@ class PostController extends Controller
         $categoryId = Category::where('name', $categoryName)->pluck('id')->first();
         if ($categoryId)
         {
-
-            $post = auth()->user()->posts()->create($credentials);
+            $user = auth()->user();
+            $post = $user->posts()->create($credentials);
             $post->category()->sync([$categoryId]);
             
             return response()->json(['message'=> 'Post created successfully']);
