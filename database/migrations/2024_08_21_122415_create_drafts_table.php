f1c8651de608ae23947ff57b6166782044e0cfdd
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-21 13:45:55 +0100

    draft table creation

---
+++ database/migrations/2024_08_21_122415_create_drafts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drafts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('blog_image');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('category')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('drafts', function (Blueprint $table) {
+            Schema::dropIfExists('drafts');
+        });
+    }
+};
