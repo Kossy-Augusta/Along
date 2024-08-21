61ad5f8cc124483bbdb7637bd740295b2247b800
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-21 15:53:01 +0100

    created a resource for draft index resource

---
+++ app/Http/Resources/DraftIndexResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DraftIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'post_date' => $this->updated_at,
+            'category' => $this->category,
+            'title' => $this->title
+        ];;
+    }
+}
