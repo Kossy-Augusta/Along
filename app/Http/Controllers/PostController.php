1f2525a628e88eb87495009290e7dbd67e82da4e
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-21 09:50:04 +0100

    added index method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,18 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Middleware\AccountTYpe;
-use App\Models\Category;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
+use App\Http\Middleware\AccountTYpe;
+use App\Http\Requests\UpdateRequest;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\CreatePostRequest;
+use App\Http\Resources\UserPostsResource;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $user = Auth::user();
+        $posts = new Post;
+        $post = $posts->where('user_id', $user->id )->get();
+        $count = $post->count();
+        return response()->json(['post_count' => $count, 'posts' => UserPostsResource::collection($post), ]);
+
+    }
     public function store(CreatePostRequest $request)
     {
-        $credentials = $request->only('title', 'description', 'blog_image', "status");
+        $credentials = $request->only('title', 'description', 'blog_image', "status", "category");
         $image = $credentials['blog_image'];
         $path = $image->store('blog_images', 'public');
         $credentials['blog_image'] = $path;
@@ -31,5 +44,15 @@ class PostController extends Controller
         {
             return response()->json(['message'=> 'Post not created, Category does not exist']);
         }
+    }
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return response()->json(['product' => $post, Response:: HTTP_ACCEPTED]);
+    }
+
+    public function update($id, UpdateRequest $request)
+    {
+
     }
 }
