b4da18edde5080aeeb979222c4c846764bdb165d
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-23 10:30:53 +0100

    resouce to return details of a blog

---
+++ app/Http/Resources/SingleCategoryPostResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SingleCategoryPostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'blog_image' => asset('storage/' . $this->blog_image)
+        ];
+    }
+}
