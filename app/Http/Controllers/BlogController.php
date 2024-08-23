1d29e9523308426182fedb91619ce337e3e62099
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-23 22:05:40 +0100

    added method for creating a comment

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Comment;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Traits\ResponseWithHttp;
+use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\CategoryResource;
-use App\Http\Resources\SingleCategoryPostResource;
 use App\Http\Resources\SinglePostResource;
+use App\Http\Resources\SingleCategoryPostResource;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class BlogController extends Controller
@@ -47,5 +52,24 @@ class BlogController extends Controller
     return response()->json(['data' => $data]);
     }
     
-    
+    /**
+     * make a comment
+     * 
+     */
+    public function createComment(Request $request, $post_id)
+    {
+        $user = Auth::user();
+        $comment = $request->validate([
+            'description'  => 'required|string'
+        ]);
+        try {
+            $new_comment = $user->comments()->create([
+                'post_id' => $post_id,
+                'description' => $comment['description']
+            ]);
+            return $this->success('Comment created successfully', $new_comment);
+        } catch (\Throwable $th) {
+            return $this->failure('Unable to create new comment');
+        }
+    }
 }
