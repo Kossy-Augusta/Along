7421f31cae97a268dd5054c599dc833d6aa871ec
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-23 12:30:25 +0100

    fix

--- app/Http/Resources/SingleCategoryPostResource.php
+++ app/Http/Resources/SingleCategoryPostResource.php
@@ -15,6 +15,7 @@ class SingleCategoryPostResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'blog_image' => asset('storage/' . $this->blog_image)
