2c0f597fa086fcac8c76d78e190836eff3d2e253
Kossy-Augusta <kossyudogu@gmail.com>
2024-08-25 21:45:56 +0100

    fix

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -15,7 +15,7 @@ use App\Http\Resources\CategoryResource;
 use App\Http\Resources\CommentResource;
 use App\Http\Resources\SinglePostResource;
 use App\Http\Resources\SingleCategoryPostResource;
-use Illuminate\Contracts\Database\Eloquent\Builder;
+
 
 class BlogController extends Controller
 { 
@@ -52,40 +52,4 @@ class BlogController extends Controller
     return response()->json(['data' => $data]);
     }
     
-    /**
-     * make a comment
-     * 
-     */
-    public function createComment(Request $request, $post_id)
-    {
-        $user = Auth::user();
-        $comment = $request->validate([
-            'description'  => 'required|string'
-        ]);
-        try {
-            $new_comment = $user->comments()->create([
-                'post_id' => $post_id,
-                'description' => $comment['description']
-            ]);
-            return $this->success('Comment created successfully', $new_comment);
-        } catch (\Throwable $th) {
-            return $this->failure('Unable to create new comment');
-        }
-    }
-    /**
-     * Get all comments related to a post
-     */
-    public function getPostComment($id)
-    {
-        $comment = Comment::whereHas('post', function (Builder $q) use($id)
-        {
-            $q->where('post_id', $id);
-        })->get();
-        if($comment->isNotEmpty())
-        {
-
-            return response()->json(['data' => CommentResource::collection($comment)]);
-        }
-        return response('No comments available for this post');
-    }
 }
